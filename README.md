849cb1cabe2e6a92f5ededfe5d6fe11b05e2cf7f
Viveli <maceo.jalbert@epitech.eu>
2022-07-20 18:09:18 +0200

    :feat: finish javascript prog

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 Experiment with web scrapping.
 
 [link of the course](https://geekflare.com/web-scraping-in-javascript/)
+
+use this command to run the script :
+```bash
+node extract_cricket_world_cups_list.js
+```
--- extract_cricket_world_cups_list.js
+++ extract_cricket_world_cups_list.js
@@ -1,3 +1,58 @@
-const fetch = require("node-fetch");
+const axios = require('axios');
+const cheerio = require("cheerio");
 
 // function to get the raw data
+const getRawData = (URL) => {
+    return axios(URL)
+    .then((responce) => {
+        return responce.data;
+    })
+    .catch(console.error);
+};
+
+// URL for data
+const URL = "https://en.wikipedia.org/wiki/Cricket_World_Cup"
+
+// start of the program
+const getCricketWorldCupsList = async () => {
+    const cricketWorldCupRawData = await getRawData(URL);
+
+    const parsedCricketWorldCupData = cheerio.load(cricketWorldCupRawData);
+
+    const worldCupsDataTable = parsedCricketWorldCupData("table")[0].children[1].children;
+
+    console.log("Year --- Winner --- Runner");
+    worldCupsDataTable.forEach((row) => {
+        if (row.name === "tr") {
+            let year, winner, runner = null;
+
+            const columns = row.children.filter((column) => column.name === "td");
+
+            const yearColumn = columns[0];
+            if (yearColumn) {
+                year = yearColumn.children[0];
+                if (year) {
+                    year = year.data;
+                }
+            }
+            const winnerColumn = columns[3];
+            if (winnerColumn) {
+                if (winnerColumn.children[0]) {
+                    winner = winnerColumn.children[0].children[0].data;
+                }
+            }
+            const runnerColumn = columns[3];
+            if (runnerColumn) {
+                if (runnerColumn.children[0]) {
+                    runner = runnerColumn.children[0].children[0].data;
+                }
+            }
+            if (year && winner && runner) {
+                console.log(`${year} --- ${winner} --- ${runner}`);
+            }
+        }
+    });
+};
+
+// invoking the main function
+getCricketWorldCupsList();
